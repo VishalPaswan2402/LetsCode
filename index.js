d2772575a1aaeb2994c8e03184d246b26a5c3f92
Vishal Paswan <vishalvp.3405@gmail.com>
2024-09-24 20:17:57 +0530

    Added question page and pick random question

--- index.js
+++ index.js
@@ -22,12 +22,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/LetsCode')
 // Home page...
 app.get('/',(req,res)=>{
     res.render("homePage.ejs");
-})
+});
 
 // signup page...
 app.post('/LetsCode/Signup',async(req,res)=>{
     let{username,name,email,password1,password2}=req.body;
-    // console.log(username,name,email,password1,password2);
     let newUser= new allUser({username:username,name:name,email:email,password:password1});
     await newUser.save();
     res.redirect(`/LetsCode/user/${username}`);
@@ -37,59 +36,83 @@ app.post('/LetsCode/Signup',async(req,res)=>{
 app.post('/LetsCode/login',async(req,res)=>{
     let{username,password}=req.body;
     let currentUser=await allUser.findOne({username:username,password:password});
-    // console.log(currentUser);
     res.redirect(`/LetsCode/user/${currentUser.username}`);
-})
+});
 
 
 // userpage...
 app.get('/LetsCode/user/:username',async(req,res)=>{
     let{username}=req.params;
-    // console.log(username);
     let userData=await allUser.findOne({username:username});
-    // console.log(userData);
     let easyPer=((userData.easySol/12)*100).toFixed(1);
     let mediumPer=((userData.mediumSol/10)*100).toFixed(1);
     let hardPer=((userData.hardSol/8)*100).toFixed(1);
     res.render("userPage.ejs",{userData,easyPer,mediumPer,hardPer});
 });
 
+// show question...
 app.get('/LetsCode/:id/topic/:Qtype',async(req,res)=>{
     let {Qtype,id}=req.params;
-    // console.log(Qtype,id);
     let ques=await allQuestion.find({topic:Qtype});
     let userData=await allUser.findById(id);
-    // console.log(ques);
     res.render('questionPage.ejs',{ques,id,userData})
-})
+});
 
-app.put('/LetsCode/:id/:qId/solved',async(req,res)=>{
-    let {id,qId}=req.params;
-    // console.log(id,qId);
+// marked sas done...
+app.put('/LetsCode/:id/:qId/solved/:solvedType',async(req,res)=>{
+    let {id,qId,solvedType}=req.params;
+    // console.log(solvedType);
     let findData=await allUser.findById(id);
-    // console.log(findData);
     let findQue=await allQuestion.findById(qId);
-    console.log(findQue.topic);
     let qLevel=findQue.level;
     let solvedTotal=findData.solvedQues+1;
     if(qLevel==="easy"){
         let solved=findData.easySol+1;
         let updateData=await allUser.findByIdAndUpdate(id,{solvedQues:solvedTotal,easySol:solved,$push: { easyId: qId }});
         updateData.save();
-        // console.log(updateData);
     }else if(qLevel==="medium"){
         let solved=findData.mediumSol+1;
         let updateData=await allUser.findByIdAndUpdate(id,{solvedQues:solvedTotal,mediumSol:solved,$push: { mediumId: qId }});
         updateData.save();
-        // console.log(updateData);
     }else{
         let solved=findData.hardSol+1;
         let updateData=await allUser.findByIdAndUpdate(id,{solvedQues:solvedTotal,hardSol:solved,$push: { hardId: qId }});
         updateData.save();
-        // console.log(updateData);
     }
-    // console.log(findData);
-    res.redirect(`/LetsCode/${id}/topic/${findQue.topic}`)
+    if(solvedType==="topicSolved"){
+        res.redirect(`/LetsCode/${id}/topic/${findQue.topic}`)
+    }else{
+        res.redirect(`/LetsCode/user/${findData.username}`)
+    }
+});
+
+//random question...
+app.put('/LetsCode/randomQue/Pick/:id',async(req,res)=>{
+    let {id}=req.params;
+    let{qLevel}=req.body;
+    let randomQuestionId;
+    if(qLevel==='random'){
+        let allQue=await allQuestion.find();
+        let randomNum=Math.floor((Math.random()*allQue.length));
+        randomQuestionId=allQue[randomNum]._id;
+    }
+    else{
+        let allQue=await allQuestion.find({level:qLevel});
+        let randomNum=Math.floor((Math.random()*allQue.length));
+        randomQuestionId=allQue[randomNum]._id;
+    }
+    // res.render(`randomQuestionPage.ejs`,{randomQuestion,userData});
+    res.redirect(`/LetsCode/randomQue/${randomQuestionId}/Picked/user/${id}/`)
+});
+
+// random Question page render...
+app.get('/LetsCode/randomQue/:qId/Picked/user/:id/',async(req,res)=>{
+    let{qId,id}=req.params;
+    // console.log(qId,id);
+    let userData=await allUser.findById(id);
+    let randomQuestion=await allQuestion.findById(qId);
+    // console.log(randomQuestion);
+    res.render(`randomQuestionPage.ejs`,{randomQuestion,userData});
 })
 
 app.listen(port, () => {
---
+++ public/inputValidate.js
@@ -0,0 +1,19 @@
+(() => {
+    'use strict'
+    const forms = document.querySelectorAll('.needs-validation')
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+
+  // Flash Time Out...
+  setTimeout(function(){
+    document.getElementById('flashId').style.display = 'none';
+  }, 3000);
