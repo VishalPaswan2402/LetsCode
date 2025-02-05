37c2db80318f8e3d14a2e7882943ef0e7dd47243
Vishal Paswan <vishalvp.3405@gmail.com>
2025-02-05 14:05:48 +0530

    session store

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(cookieParser());
 const sessionOption = {
     secret: process.env.SECREAT_PASS || 'letscodeindia',
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
